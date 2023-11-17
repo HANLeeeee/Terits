837fc00b3784a4d0b4c05dc9365c37cc089ecd53
HANLeeeee <hanleeeee@naver.com>
2023-11-17 23:44:59 +0900

    Initial Commit

---
+++ Terits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Terits
+//
+//  Created by 최하늘 on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
